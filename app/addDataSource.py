834cf45f8b61201086747b515928f772655d65e4
Piotr Karpala <karpik.pl@gmail.com>
2018-02-25 09:50:19 -0500

    delete route

--- app/addDataSource.py
+++ app/addDataSource.py
@@ -1,5 +1,5 @@
 from app import app, get_mongo
-from flask import jsonify, abort, request
+from flask import abort, request
 from tools import JSONEncoder
 from bson.objectid import ObjectId
 
@@ -8,7 +8,7 @@ from bson.objectid import ObjectId
 def add_data_source():
 
     if not request.json or not 'url' in request.json or not 'name' in request.json:
-        abort(400)
+        abort(400, f'provided json payload was not correct: {request.json}')
     dataSource = {
         'name': request.json['name'],
         'url': request.json['url']
--- app/deleteDataSource.py
+++ app/deleteDataSource.py
@@ -1,9 +1,16 @@
-from app import app
+from app import app, get_mongo
 from flask import jsonify, abort
+from bson.objectid import ObjectId
 
 
 @app.route('/dataSource/<string:id>', methods=['DELETE'])
 def delete_data_source(id):
-    if not id or id == "1":
-        abort(404)
-    return jsonify({'task deleted': id})
+    if not id:
+        abort(404, 'id not provided')
+
+    result = get_mongo().db.dataSources.delete_one({'_id': ObjectId(id)})
+
+    if result.deleted_count is 1:
+        return f'deleted {id}', 202
+    else:
+        abort(404, f'data source with id {id} not found!')
